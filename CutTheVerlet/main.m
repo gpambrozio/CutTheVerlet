f706dc70fd9a9fb7fbdcbd23dcfb4803fa1912b4
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2012-06-02 11:55:56 -0300

    Initial Commit

---
+++ CutTheVerlet/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CutTheVerlet
+//
+//  Created by Gustavo Ambrozio on 2/6/12.
+//  Copyright OpenTheJob 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
